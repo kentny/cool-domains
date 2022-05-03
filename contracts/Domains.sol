2c63f58aec428e4a0dfe986c007587fecf502e6a
Kentaro Terasaki <kterasaki@vmware.com>
2022-05-03 09:44:15 +0900

    Add some code.

---
+++ contracts/Domains.sol
@@ -0,0 +1,12 @@
+// PSDX-License-Identifier: UNLICENSED
+
+// pragma solidity ^0.8.10;
+pragma solidity ^0.8.10;
+
+import "hardhat/console.sol";
+
+contract Domains {
+    constructor() {
+        console.log("THIS IS DOMAINS CONTRACT. NICE.");
+    }
+}
