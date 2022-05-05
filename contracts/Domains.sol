9a3a819198982e2900e8ba5ed0326351a499749d
Kentaro Terasaki <kterasaki@vmware.com>
2022-05-05 09:31:50 +0900

    Add the functionality of domain record.

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -6,7 +6,33 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
+    mapping(string => address) public domains;
+    mapping(string => string) public records;
+
     constructor() {
         console.log("THIS IS DOMAINS CONTRACT. NICE.");
+    }
+
+    function register(string calldata name) public {
+        require(domains[name] == address(0));
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain.", msg.sender);
+    }
+
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name)
+        public
+        view
+        returns (string memory)
+    {
+        return records[name];
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,26 @@
 const main = async () => {
+    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
     await domainContract.deployed();
     console.log("Contract deployed to:", domainContract.address);
+    console.log("Contract deployed by:", owner.address);
+
+    let txn = await domainContract.register("doom");
+    await txn.wait();
+
+    const domainOwner = await domainContract.getAddress("doom");
+    console.log("Owner of domain:", domainOwner);
+
+    // This code is failed because `randomPerson` is not the owner of the domain.
+    // txn = await domainContract.connect(randomPerson).setRecord("doom", "This is my domain now!");
+    // await txn.wait();
+    
+    txn = await domainContract.setRecord("doom", "HELLO!!! How are you??");
+    await txn.wait();
+
+    const domainRecord = await domainContract.getRecord("doom");
+    console.log("Record of domain:", domainRecord);
 }
 
 const runMain = async () => {
