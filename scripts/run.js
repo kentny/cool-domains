cf7b2acafce3d5f889cc9016c5592636da024d9e
Kentaro Terasaki <kterasaki@vmware.com>
2022-05-05 10:18:46 +0900

    Add a specific domain like .chocolate.

--- scripts/run.js
+++ scripts/run.js
@@ -1,26 +1,18 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
+    const domainContract = await domainContractFactory.deploy("chocolate");
     await domainContract.deployed();
-    console.log("Contract deployed to:", domainContract.address);
-    console.log("Contract deployed by:", owner.address);
 
-    let txn = await domainContract.register("doom");
+    console.log("Contract deployed to:", domainContract.address);
+
+    let txn = await domainContract.register("black", {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
 
-    const domainOwner = await domainContract.getAddress("doom");
+    const domainOwner = await domainContract.getAddress("black");
     console.log("Owner of domain:", domainOwner);
 
-    // This code is failed because `randomPerson` is not the owner of the domain.
-    // txn = await domainContract.connect(randomPerson).setRecord("doom", "This is my domain now!");
-    // await txn.wait();
-    
-    txn = await domainContract.setRecord("doom", "HELLO!!! How are you??");
-    await txn.wait();
-
-    const domainRecord = await domainContract.getRecord("doom");
-    console.log("Record of domain:", domainRecord);
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 }
 
 const runMain = async () => {
